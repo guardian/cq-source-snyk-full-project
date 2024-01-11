357eaea1e52ba13a339e59059b7dda312a942bc9
Chris Jones <chrislomaxjones96@gmail.com>
2024-01-11 15:29:37 +0000

    WIP instantiating client
    Co-authored-by: TJ Silver <thalia.silver@guardian.co.uk>
    Co-authored-by: Natasha <67543397+NovemberTang@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-	"github.com/guardian/cq-source-snyk-full-project/plugin"
+	"context"
+	"fmt"
 
-	"github.com/cloudquery/plugin-sdk/v3/serve"
+	openapi "github.com/guardian/cq-source-snyk-full-project/generatedclient"
 )
 
+// import (
+// 	"github.com/guardian/cq-source-snyk-full-project/plugin"
+
+// 	"github.com/cloudquery/plugin-sdk/v3/serve"
+// )
+
 func main() {
-	serve.Source(plugin.Plugin())
+	fmt.Println("Hello world!")
+
+	auth := context.WithValue(
+		context.Background(),
+		openapi.ContextAPIKeys,
+		map[string]openapi.APIKey{
+			"Authorization": {Key: "API_KEY_STRING"},
+		},
+	)
+
+	client := openapi.NewAPIClient(openapi.NewConfiguration())
+
+	x := client.ProjectsAPI.ListOrgProjects(auth, "foobar")
+	fmt.Println(x)
+
+	// serve.Source(plugin.Plugin())
 }
